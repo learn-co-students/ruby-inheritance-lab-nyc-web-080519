d9776a9557daf2a872a29e658ce636b55e2d4fc1
tomjmcnamee <tom@tomjmcnamee.com>
2019-08-11 11:20:46 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end  # ends initialize method
+
+  def learn(knowl_string)
+    self.knowledge << knowl_string
+  end  # ends learn method
+
+  def knowledge
+    @knowledge
+  end  # ends knowledge method
 
 end
